870e2890d18e88bc76c8fa6176c02edfe1c379e8
Renan Albuquerque <renan.albuquerque2501@gmail.com>
2024-03-27 16:18:33 -0300

    arquivo atualizados

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  entry: 'index.js', // Arquivo de entrada principal
+  entry: 'main.jsx', // Arquivo de entrada principal
   output: {
     path: path.resolve(__dirname, 'dist'), // Diretório de saída para os arquivos gerados
-    filename: 'bundle.js' // Nome do arquivo de saída
+    filename: 'index.html' // Nome do arquivo de saída
   },
   module: {
     rules: [

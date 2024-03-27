b23f09df70e4e013fbf1a575e7c08d4a1f6f89b1
Renan Albuquerque <renan.albuquerque2501@gmail.com>
2024-03-27 16:05:19 -0300

    arquivo atualizado

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -6,18 +6,4 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'), // Diretório de saída para os arquivos gerados
     filename: 'bundle.js' // Nome do arquivo de saída
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/, // Aplica as regras apenas aos arquivos JS
-        exclude: /node_modules/, // Exclui o diretório node_modules
-        use: {
-          loader: 'babel-loader', // Usa o Babel para transpilar o código
-          options: {
-            presets: ['@babel/preset-env'] // Conjunto de presets do Babel
-          }
-        }
-      }
-    ]
-  }
 };
